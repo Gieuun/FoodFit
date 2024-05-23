4fe57a03a97d9507bdfa658086b94b5db49298c3
gieeun <gieun0528@gamil.com>
2024-05-23 14:27:22 +0900

    first commit

---
+++ src/main/java/com/sds/foodfit/FoodFitApplication.java
@@ -0,0 +1,13 @@
+package com.sds.foodfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sds/foodfit/FoodFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sds.foodfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
