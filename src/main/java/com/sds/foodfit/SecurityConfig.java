3e836ecdaae3396fc2140ded6527a332517f75b7
chagrace <cwg126@naver.com>
2024-05-25 11:28:47 +0900

    notice

--- src/main/java/com/sds/foodfit/SecurityConfig.java
+++ src/main/java/com/sds/foodfit/SecurityConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SecurityConfig {
