6196cb20d47845998564913a413f813a39f59e3a
maker-ymc <young8@knou.ac.kr>
2024-05-29 22:41:14 +0900

    금일 작업 업로드 (DAO,Service), 사이트

--- src/main/java/com/sds/foodfit/domain/Allergy.java
+++ src/main/java/com/sds/foodfit/domain/Allergy.java
@@ -5,8 +5,8 @@ import lombok.Data;
 @Data
 public class Allergy {
 	
-	private int allergy_idx;
-	private String allergy_name;			 // 알러지 이름
+	private int allergyIdx;
+	private String allergyName;			 // 알러지 이름
 	private MemberDetail memberDetail; // has a 관계로 회원추가 정보 객체 가짐
 	private Member member;				 // has a 관계로 회원 정보 객체 가짐
 }
--- src/main/java/com/sds/foodfit/domain/Notice.java
+++ src/main/java/com/sds/foodfit/domain/Notice.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class Notice {
-	private int notice_idx;
+	private int noticeIdx;
 	private String title;
 	private String writer;
 	private String content;
