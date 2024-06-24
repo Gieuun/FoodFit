59a1fdd02695874aa4764d5b3bf286bd2918885b
maker-ymc <young8@knou.ac.kr>
2024-06-24 14:29:45 +0900

    로그인 유지오류 수정 (LoginFilter에 수정내용 각주추가함)

--- src/main/java/com/sds/foodfit/SecurityConfig.java
+++ src/main/java/com/sds/foodfit/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -37,30 +36,22 @@ public class SecurityConfig {
 
     // 시큐리티 필터체인 객체 호출 (접근허가 관련 작업)
     @SuppressWarnings("deprecation")
-	@Bean
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
 	http.authorizeHttpRequests((authorize) -> authorize
 
 		.requestMatchers("/site/**").permitAll() // 모든 요청을 인증 없이 허용.
 		.requestMatchers("/img/**").permitAll() // 모든 요청을 인증 없이 허용.
-		.requestMatchers("/**").permitAll() // 모든 요청을 인증 없이 허용. 결과물 나오면 수정필요!
+		.requestMatchers("/").permitAll() // 모든 요청을 인증 없이 허용.
 		.requestMatchers("/recomember/login", "/recomember/loginform").permitAll()
 		.requestMatchers("/recomember/regist", "/recomember/registform").permitAll()
-		
-		.requestMatchers("/").permitAll() // 모든 요청을 인증 없이 허용.
-		.requestMatchers("/recofood/**").permitAll() // 음식추천경로를 인증 없이 허용. 결과물 나오면 수정필요!
-		.requestMatchers("/food/**").permitAll() // 음식추천요청을 인증 없이 허용. 결과물 나오면 수정필요!
-		.requestMatchers("/favoritefood/**").permitAll() // 즐겨찾기 호출 허용
-		.requestMatchers("/rest/favoritefood/**").permitAll() // 즐겨찾기 rest 호출 허용
-		.requestMatchers("/rest/foodTypeSurvey/regist").permitAll() // 서베이등록post허용
-
-		.requestMatchers("/rest/notice/**").permitAll() // 모든 요청을 인증 없이 허용. 결과물 나오면 수정필요!
+		.requestMatchers("/notice/**").permitAll().requestMatchers("/rest/notice/**").permitAll()
 		.requestMatchers("/recomember/sns/**").permitAll() // sns 이용자 요청 허용
-		.requestMatchers("/rest/recomember/**").permitAll()
-
-		//.requestMatchers("/recomember/mypage").hasAnyAuthority("USER")
-		
+		.requestMatchers("/rest/recomember/**").permitAll() // rest요청
+		.requestMatchers("/recomember/mypage").hasAnyAuthority("USER") // 마이페이지
+		.requestMatchers("/recofood/**").hasAnyAuthority("USER") // 회원만 이용
+		.requestMatchers("/recomember/mypage").hasAnyAuthority("USER") // 회원만 이용
 
 		.anyRequest().authenticated() // 그 외의 요청은 인증 필요
 	);
@@ -73,23 +64,21 @@ public class SecurityConfig {
 	 * setFilterProcessesUrl("member/login"); 를 등록해야한다.
 	 * ===================================
 	 */
-	
+
 	http.formLogin(login -> login.disable());
-	 
-	
-	
+
 	http.logout(logout -> logout.logoutUrl("/logout") // 로그아웃요청 URL
-			.logoutSuccessUrl("/") // 로그아웃 성공 후 리다이렉트할 URL
-			.invalidateHttpSession(true) // HTTP 세션을 무효화할지 여부
-			.deleteCookies("JSESSIONID") // 로그아웃 시 삭제할 쿠키 이름 설정
-		);
-	
-	
+		.logoutSuccessUrl("/") // 로그아웃 성공 후 리다이렉트할 URL
+		.invalidateHttpSession(true) // HTTP 세션을 무효화할지 여부
+		.deleteCookies("JSESSIONID") // 로그아웃 시 삭제할 쿠키 이름 설정
+	);
+
 	http.httpBasic(httpBasic -> httpBasic.realmName("FoodFit") // 기본 인증 사용 시 realm 이름 설정
 	);
 
 	http.addFilterAt(loginFilter(), UsernamePasswordAuthenticationFilter.class);
-	//http.addFilterAfter(new SecurityContextPersistenceFilter(), LoginFilter.class);
+	// http.addFilterAfter(new SecurityContextPersistenceFilter(),
+	// LoginFilter.class);
 
 	return http.build();
     }
