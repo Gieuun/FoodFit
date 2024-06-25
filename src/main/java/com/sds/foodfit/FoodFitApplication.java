6d0099df3f0a8383ed5b17a6c35c2fc50eb18553
gieeun <gieun0528@gamil.com>
2024-06-25 10:04:41 +0900

    war 플러그인 설정

--- src/main/java/com/sds/foodfit/FoodFitApplication.java
+++ src/main/java/com/sds/foodfit/FoodFitApplication.java
@@ -2,12 +2,13 @@ package com.sds.foodfit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class FoodFitApplication {
+public class FoodFitApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoodFitApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(FoodFitApplication.class, args);
+    }
 }
